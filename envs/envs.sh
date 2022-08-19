510c91bf34c986f5965318cf81be63449558127e
Janusz Koszucki <jkoszucki@jkoszucki.mcb.bio>
2022-08-19 20:33:34 +0200

    setup envs

---
+++ envs/envs.sh
@@ -0,0 +1,7 @@
+conda create -n mashtree -c conda-forge mamba
+conda activate mashtree
+mamba install -c bioconda mashtree
+
+conda create -n mybase -c conda-forge mamba
+conda activate mybase
+mamba install -c conda-forge -c bioconda jupyterlab matplotlib pandas pathlib  markov_clustering biopython networkx sklearn-pandas pyvips pillow pyyaml
